1ebcec98714adac5da92807be676ddceb5e89d70
Mark Watts <mark.watts@hotmail.co.uk>
2013-04-19 09:35:51 +0100

    Null module and tests.

---
+++ src/HandyFS.Tests/null-facts.fs
@@ -0,0 +1,38 @@
+﻿module ``Null facts`` 
+
+    open System
+    open FsUnit.Xunit
+    open Xunit
+    open Null
+
+    type ``asOption function`` () =
+
+        [<Fact>] member test.
+            ``Returns None when value is null`` () =
+                let 
+                    value = Nullable<int> ()
+                in
+                    value |> asOption |> ((=) None) |> should be True
+        
+        [<Fact>] member test.
+            ``Returns Some when value is not null`` () =
+                let
+                    value = Nullable (10)
+                in
+                    value |> asOption |> should equal (Some 10)
+
+    type ``asDefault function`` () =
+
+        [<Fact>] member test.
+            ``Returns the type default when value is None`` () =
+                let
+                    value : int option = None
+                in
+                    value |> asDefault |> should equal 0
+
+        [<Fact>] member test.
+            ``Returns the wrapped value when the value is Some`` () =
+                let
+                    value = Some 10
+                in
+                    value |> asDefault |> should equal 10
---
+++ src/HandyFS/null.fs
@@ -0,0 +1,18 @@
+﻿///Contains functions for working with null values
+module Null
+
+    open System
+
+    ///Converts a nullable value to an option
+    let asOption (value : Nullable<_>) = 
+        if value.HasValue then
+            Some value.Value
+        else    
+            None
+
+    ///Converts an option to a definite value, using the type's default when the value is None
+    let asDefault<'a> (value : 'a option) = 
+        match value with
+        | None -> Unchecked.defaultof<'a>
+        | Some value' -> value'
+        
