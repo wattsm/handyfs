379e078308e5769e2e45f6ebb5b58530f5b363be
Mark Watts <mark.watts@hotmail.co.uk>
2013-05-22 16:11:17 +0100

    Additional date/time functions, including startOfWeek and getDaysInMonth, plus tests.

--- src/HandyFS.Tests/datetime-facts.fs
+++ src/HandyFS.Tests/datetime-facts.fs
@@ -6,28 +6,67 @@
 
     let datetime = DateTime (2013, 4, 17, 13, 50, 0)
 
-    type ``Midnight method`` () =
+    module ``Time Module`` =
 
-        [<Fact>] member test.
-            ``Returns midnight of the date`` () =
-                datetime.Midnight () |> should equal (DateTime (2013, 4, 17, 0, 0, 0))
+        type ``At method`` () =
 
-    type ``Next method`` () =
+            [<Fact>] member test.
+                ``Returns the given time of the date`` () =
+                    datetime.At 9 30 15 |> should equal (DateTime (2013, 4, 17, 9, 30, 15))
 
-        [<Fact>] member test.
-            ``Returns the correct date when day is in current week`` () =
-                datetime.Next (DayOfWeek.Friday) |> should equal (DateTime (2013, 4, 19, 13, 50, 0))
+        type ``Midnight method`` () =
 
-        [<Fact>] member test.
-            ``Returns the correct date when day is in next week`` () =
-                datetime.Next (DayOfWeek.Tuesday) |> should equal (DateTime (2013, 4, 23, 13, 50, 0))
+            [<Fact>] member test.
+                ``Returns midnight of the current date`` () =
+                    datetime.Midnight () |> should equal (DateTime (2013, 4, 17, 0, 0, 0))
 
-    type ``Previous method`` () =
+    module ``Date Module`` = 
 
-        [<Fact>] member test.
-            ``Returns the correct date when the day is in the current week`` () =
-                datetime.Previous (DayOfWeek.Tuesday) |> should equal (DateTime (2013, 4, 16, 13, 50, 0))
+        type ``GetDaysInMonth month`` () = 
 
-        [<Fact>] member test.
-            ``Returns the correct date when the day is in the previous week`` () =
-                datetime.Previous (DayOfWeek.Friday) |> should equal (DateTime (2013, 4, 12, 13, 50, 0))
+            [<Fact>] member test.
+                ``Returns the correct list of dates`` () =  
+                    let 
+                        expected = 
+                            [ 1; 8; 15; 22; 29; ]
+                            |> List.map (fun day -> DateTime (2013, 4, day))
+                    in
+                        Date.getDaysInMonth 2013 4 DayOfWeek.Monday
+                        |> should equal expected
+
+    module ``DateTime Extensions`` = 
+
+        type ``Next method`` () =
+
+            [<Fact>] member test.
+                ``Returns the correct date when the next occurrence of the reqested day is in current week`` () =
+                    datetime.Next (DayOfWeek.Friday) |> should equal (DateTime (2013, 4, 19, 13, 50, 0))
+
+            [<Fact>] member test.
+                ``Returns the correct date when next occurrence of the requested day is in next week`` () =
+                    datetime.Next (DayOfWeek.Tuesday) |> should equal (DateTime (2013, 4, 23, 13, 50, 0))
+
+        type ``Previous method`` () =
+
+            [<Fact>] member test.
+                ``Returns the correct date when the previous occurrence of the requested day is in the current week`` () =
+                    datetime.Previous (DayOfWeek.Tuesday) |> should equal (DateTime (2013, 4, 16, 13, 50, 0))
+
+            [<Fact>] member test.
+                ``Returns the correct date when the previous occurrence of the requested day is in the previous week`` () =
+                    datetime.Previous (DayOfWeek.Friday) |> should equal (DateTime (2013, 4, 12, 13, 50, 0))
+
+        type ``StartOfWeek method`` () =
+
+            let monday = 
+                DateTime (2013, 4, 15, 13, 50, 0)
+
+            [<Fact>] member test.
+                ``Returns the correct date when the date is a Monday`` () =
+                    monday.StartOfWeek () |> should equal monday
+
+            [<Fact>] member test.
+                ``Returns the correct date then when date is not a Monday`` () =
+                    datetime.StartOfWeek () |> should equal monday
+
+    
--- src/HandyFS/datetime.fs
+++ src/HandyFS/datetime.fs
@@ -4,34 +4,112 @@ module DateTime
 
     open System
 
-    type DateTime with
+    ///Contains functions for manipulating times
+    [<RequireQualifiedAccess>]
+    module Time = 
 
-        ///Returns midnight of the current date
-        member this.Midnight () =
-            DateTime (this.Year, this.Month, this.Day, 0, 0, 0)
+        ///Sets the time of a given datetime
+        let at hour min sec (datetime : DateTime) = 
+            DateTime (datetime.Year, datetime.Month, datetime.Day, hour, min, sec)
 
-        ///Gets the date of the next occurrence of a day of the week
-        member this.Next (day : DayOfWeek) = 
+        ///Sets the given datetime to midnight
+        let midnight = 
+            at 0 0 0
 
+    ///Contains functions for manipulating dates
+    [<RequireQualifiedAccess>]
+    module Date = 
+
+        ///Adds a given number of days to a date
+        let addDays (num : int) (datetime : DateTime) = 
+            datetime.AddDays (float num)
+
+        ///Sets the next day of the week for a given datetime
+        let next day (datetime : DateTime) = 
+            
             let targetDay = (int day)
-            let currentDay = (int this.DayOfWeek)
+            let currentDay = (int datetime.DayOfWeek) 
 
             let difference =
                 match (targetDay - currentDay) with
                 | d when d < 0 -> d + 7
                 | d -> d              
 
-            this.AddDays (float difference)
+            datetime.AddDays (float difference)
 
-        ///Gets the date of the previous occurrence of a day of the week
-        member this.Previous (day : DayOfWeek) = 
+        ///Sets the next day of the week for a given datetime, excluding the current day
+        let exclusiveNext day = 
+            addDays 1
+            >> next day
+
+        ///Sets the previous day of the week for a given datetime
+        let previous day (datetime : DateTime) = 
 
             let targetDay = (int day)
-            let currentDay = (int this.DayOfWeek)
+            let currentDay = (int datetime.DayOfWeek)
 
             let difference = 
                 match (targetDay - currentDay) with
                 | d when d > 0 -> d - 7
                 | d -> d
 
-            this.AddDays (float difference)
+            datetime.AddDays (float difference)
+
+        ///Sets the previous day of the week for a given datetime, excluding the current day
+        let exclusivePrevious day = 
+            addDays -1
+            >> previous day
+
+        ///Sets the day of the week to be the Monday of the given date's week
+        let startOfWeek (datetime : DateTime) = 
+            if (datetime.DayOfWeek = DayOfWeek.Monday) then
+                datetime
+            else
+                datetime
+                |> previous DayOfWeek.Monday        
+
+        ///Gets the dates of all given days of the week in a month
+        let getDaysInMonth year month dayOfWeek = 
+
+            let rec checkAndProgress (date : DateTime) = 
+                if (date.Month <> month) then
+                    []
+                else
+                    date :: (date |> exclusiveNext dayOfWeek |> checkAndProgress)
+
+            DateTime (year, month, 1)
+            |> addDays -1 //End of previous month
+            |> next dayOfWeek
+            |> checkAndProgress
+            
+        ///Gets the dates of all given days of the week in the current month
+        let getDaysInCurrentMonth dayOfWeek = 
+            getDaysInMonth DateTime.Now.Year DateTime.Now.Month dayOfWeek
+
+    ///Extension methods for the the DateTime type
+    type DateTime with
+
+        ///Returns the current date at the given time
+        member this.At hour min sec = 
+            this
+            |> Time.at hour min sec
+
+        ///Returns midnight of the current date
+        member this.Midnight () =
+            this 
+            |> Time.midnight
+
+        ///Gets the date of the next occurrence of a day of the week
+        member this.Next (day : DayOfWeek) = 
+            this
+            |> Date.next day
+
+        ///Gets the date of the previous occurrence of a day of the week
+        member this.Previous (day : DayOfWeek) = 
+            this
+            |> Date.previous day
+
+        ///Sets the day of the week to be the Monday of the date's week
+        member this.StartOfWeek () =
+            this
+            |> Date.startOfWeek
