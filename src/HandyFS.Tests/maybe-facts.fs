4312bd613263054000b5bf4365926839160cf2a8
Mark Watts <mark.watts@hotmail.co.uk>
2013-05-31 08:52:16 +0100

    Maybe monad plus tests.

---
+++ src/HandyFS.Tests/maybe-facts.fs
@@ -0,0 +1,53 @@
+﻿module ``Maybe facts``
+
+    open System
+    open FsUnit.Xunit
+    open Xunit
+    open HandyFS.Maybe
+
+    module ``bind function`` =
+
+        let dummy (num : Int32) = 
+            Some (num * 2)
+
+        let [<Fact>] ``Function is called when value is not None`` () =
+            bind None dummy |> Option.isNone |> should be True
+
+        let [<Fact>] ``Function is not called when value is None`` () =
+            bind (Some 2) dummy |> Option.get |> should equal 4
+
+    module ``Monad laws`` =
+
+        let assertEquality (f : Int32 -> Int32 option) (g : Int32 -> Int32 option) = 
+
+            let fv = f 2 |> Option.get
+            let gv = g 2 |> Option.get
+
+            Assert.Equal (fv, gv)
+
+        let isEven n = n % 2 = 0        
+        let incrementEven n = if (isEven n) then Some (n + 1) else None
+        let incrementOdd n = if not (isEven n) then Some (n + 1) else None
+        let doubleEven n = if (isEven n) then Some (n * 2) else None
+        let (-->) f g = bind f g
+
+        let [<Fact>] ``return a >>= f = f a`` () =
+
+            let f n = return' n --> incrementEven
+            let g = incrementEven
+
+            assertEquality f g
+
+        let [<Fact>] ``f >>= return = f`` () = 
+
+            let f n = incrementEven n --> return'
+            let g = incrementEven
+
+            assertEquality f g
+
+        let [<Fact>] ``f >>= (fun x -> g x >>= h) = (f >>= g) >>= h)`` () = 
+
+            let f n = incrementEven n --> (fun n' -> incrementOdd n' --> doubleEven)
+            let g n = incrementEven n --> incrementOdd --> doubleEven
+
+            assertEquality f g
---
+++ src/HandyFS/maybe.fs
@@ -0,0 +1,24 @@
+﻿module HandyFS.Maybe
+
+    ///Calls f with the given value so long as it is not none
+    let bind value f = 
+        match value with
+        | Some x -> f x
+        | None -> None
+
+    ///Lifts a value to the default form for the maybe monad
+    let return' x = 
+        Some x
+
+    ///Workflow builder for the maybe monad
+    type MaybeBuilder () = 
+
+        member this.Bind (expr, rest) = 
+            bind expr rest
+
+        member this.Return x = 
+            return' x
+
+    ///Workflow sugar for the maybe monad
+    let maybe = 
+        MaybeBuilder ()
